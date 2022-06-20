17c36a738298b0e1248a9f8a1add078a0817d4ae
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 23:00:27 +0200

    Added ability to init NetworkClient with an socket

--- network.py
+++ network.py
@@ -1,16 +1,25 @@
 import socket
 from enum import Enum, auto
+import json
+import time
 
 
 class NetworkClient:
 
-    def __init__(self, host, port):
+    def __init__(self, host = socket.gethostname(), port = 20550, client_socket = None, client_id = None):
+        if not client_socket:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = host
+            self.host = host
         self.port = port
-        self.addr = (self.server, self.port)
+            self.addr = (self.host, self.port)
         self.is_connected = False
         self.connect()
+        else:
+            self.client = client_socket
+            self.addr = client_socket.getpeername()
+            self.host = self.addr[0]
+            self.port = self.addr[1]
+            self.is_connected = True
 
     def connect(self):
         try:
